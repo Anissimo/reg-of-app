974cfaecaa1af0d61434e995d7430932d5b51256
grisha <grisaanisimov@gmail.com>
2023-09-04 01:17:18 +0400

    изменение иконок у журнала заявок и карты

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,39 @@
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer expand-on-hover rail>
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjhQa4VP5dD6-xO9Ank6auvRE2MHaN3SAUmQ&usqp=CAU"
-            title="Grisha Anisimov"
-            subtitle="grishaanisimov@gmailcom"
-          ></v-list-item>
-        </v-list>
+  <div class="app">
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer expand-on-hover rail>
+          <v-list>
+            <v-list-item
+              prepend-avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjhQa4VP5dD6-xO9Ank6auvRE2MHaN3SAUmQ&usqp=CAU"
+              title="Grisha Anisimov"
+              subtitle="grishaanisimov@gmailcom"
+            ></v-list-item>
+          </v-list>
 
-        <v-divider></v-divider>
+          <v-divider></v-divider>
 
-        <v-list density="compact" nav>
-          <RouterLink class="text-decoration-none" to="/statement"
-            ><v-list-item
-              prepend-icon="mdi-folder"
-              title="Journal"
-              value="Journal"
-            ></v-list-item
-          ></RouterLink>
-          <RouterLink class="text-decoration-none" to="/map"
-            ><v-list-item
-              prepend-icon="mdi-account-multiple"
-              title="Map"
-              value="Map"
-            ></v-list-item
-          ></RouterLink>
-        </v-list>
-      </v-navigation-drawer>
-      <v-main class="main"><RouterView /></v-main>
-    </v-layout>
-  </v-card>
+          <v-list density="compact" nav>
+            <RouterLink class="text-decoration-none" to="/statement"
+              ><v-list-item
+                prepend-icon="mdi-notebook"
+                title="Journal"
+                value="Journal"
+              ></v-list-item
+            ></RouterLink>
+            <RouterLink class="text-decoration-none" to="/map"
+              ><v-list-item
+                prepend-icon="mdi-compass"
+                title="Map"
+                value="Map"
+              ></v-list-item
+            ></RouterLink>
+          </v-list>
+        </v-navigation-drawer>
+        <v-main class="main"><RouterView /></v-main>
+      </v-layout>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
@@ -41,8 +43,11 @@ const tab = ref(null);
 </script>
 
 <style lang="scss" scoped>
+.app {
+  max-height: 100%;
+  overflow-y: auto;
+}
 .main {
-  position: fixed;
   top: 0;
   left: auto;
   // padding-right: 100px;
